31d061ac40534a77792fdf2a691aeca1f02a60da
sheetalksagar <sheetalksagar694@gmail.com>
2020-06-10 19:45:08 +0530

    https://github.com/sheetalksagar/onlineAngular.git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'first-my-app';
+
+  cities=[ 'Pune','Mumbai'];
 }
