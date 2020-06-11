f557c2868564930ae2ee9576bb0bf2951a308cdd
Sheetalksagar <sheetalksagar694@gmail.com>
2020-06-11 19:07:27 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,31 @@ export class AppComponent {
   title = 'first-my-app';
 
   cities=[ 'Pune','Mumbai','Nashik'];
+
+  mobParts = [
+    {
+      id: 1001,
+      name: 'Screen 5.5',
+      description: '5.5 Screen for Moto g',
+      inStock: 5,
+      prodColor: 'red',
+      canPurch: true,
+    },
+    {
+      id: 1002,
+      name: 'Screen 5',
+      description: '5 Screen for Samsung',
+      inStock: 8,
+      prodColor: 'green',
+      canPurch: false,
+    },
+    {
+      id: 1003,
+      name: 'key panel',
+      description: 'Key panel for Nokia',
+      inStock: 0,
+      prodColor: 'blue',
+      canPurch: true,
+    },
+  ];
 }
