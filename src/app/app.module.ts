43ee7f5dcab0d4f460eacffb00e4540f00841fb8
Sheetalksagar <sheetalksagar694@gmail.com>
2020-06-22 19:44:51 +0530

    product updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { RouterModule ,Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,14 @@ import { NavComponent } from './layouts/nav/nav.component';
 import { SimpleStyleDirective } from './directives/simple-style.directive';
 import { LaptopComponent } from './layouts/products/laptop/laptop.component';
 import { MobPartComponent } from './layouts/products/mob-part/mob-part.component';
+import { PageNotFoundComponent } from './layouts/page-not-found/page-not-found.component';
+
+const appRoutes : Routes = [
+  { path : "", component : MobPartComponent },
+  { path : "mobile", component : MobPartComponent },
+  { path : "laptop", component : LaptopComponent },
+  { path : "**", component : PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -28,8 +37,10 @@ import { MobPartComponent } from './layouts/products/mob-part/mob-part.component
     SimpleStyleDirective,
     LaptopComponent,
     MobPartComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule,FormsModule],
+
+  imports: [BrowserModule, AppRoutingModule,FormsModule,RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
